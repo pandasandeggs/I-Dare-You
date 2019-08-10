7ac6a421bc35f8d769187705fcd667dab009553c
pandasandeggs <theteatreehouse@gmail.com>
2019-08-09 23:33:49 -0500

    Index redirects to teams page

---
+++ app.js
@@ -0,0 +1,8 @@
+const score = 0
+const teamScore = document.getElementById("scoreboard");
+const newGameButton = document.getElementById("newGameButton");
+
+function newGameClick(newGameButton){
+	console.log("clicked inner")
+    location.href='teams.html';
+};
