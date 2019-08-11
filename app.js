23a87c8a4a8c4b7d6597e4c7bc064269fa2d3084
pandasandeggs <theteatreehouse@gmail.com>
2019-08-11 12:21:25 -0500

    Removed minutes form and added team page redirect

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-const score = 0
-const teamScore = document.getElementById("scoreboard");
-const newGameButton = document.getElementById("newGameButton");
+// const score = 0
+// const teamScore = document.getElementById("scoreboard");
 
-function newGameClick(newGameButton){
-	console.log("clicked inner")
-    location.href='teams.html';
-};
+// Index.html 
+
+const newGameButton = document.getElementById("newGameButton");
+newGameButton.addEventListener('click', function() {
+	window.location = "teams.html";
+});
+
+// Teams.html
+
+const teamSubmit = document.getElementById("submit-button");
+teamSubmit.addEventListener('click', function() {
+	const teamNumber = document.getElementById("team-form").value;
+	createScoreboard(teamNumber);
+	window.location = "dares.html";
+})
+
+// Dares.html
+
+
+// Game.html
+
+function createScoreboard(integer) {
+	console.log(integer);
+}
+
+// Rules.html
